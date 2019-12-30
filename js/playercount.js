7176a05fbad61c4c9bf3c7efa0f988ad17a47ebf
Ricardo Borsato <37642532+ricardoborsato@users.noreply.github.com>
2019-12-30 14:48:18 +0000

    Update playercount.js

--- js/playercount.js
+++ js/playercount.js
@@ -1,14 +1,15 @@
-//based on a pen done on codepen by @robinselmer
+//Based on a pen done on codepen by @robinselmer
 
-//change there the server IP. Set the IP and the port here.
+//Change there the server IP. Set the IP and the port here.
 //E.g.:
-//note use: var url = "https://api.minetools.eu/ping/<server ip>/<server port>";
+//note: use: var url = "https://api.minetools.eu/ping/<server ip>/<server port>";
 //E.g.:
 //var url = "https://api.minetools.eu/ping/myserver.net/25565";
 
 var url = "https://api.minetools.eu/ping/mc.thegrassblock.org/25565";
 $.getJSON(url, function(r) {
  if(r.error){
+     //If it can't find the server, use " 0 ". Or other word, feel free to change. =)
     $('#rest').html('0');
    return false;
  } 
