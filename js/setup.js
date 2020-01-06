7311d4354801d774db8103a41270874050a6a1c7
Ricardo Borsato <37642532+ricardoborsato@users.noreply.github.com>
2020-01-06 17:49:14 +0000

    Create setup.js

---
+++ js/setup.js
@@ -0,0 +1,16 @@
+
+/* For the Server Name */
+function servertitle() {
+  var x = document.getElementById("inputservertitle").value;
+  document.getElementById("inputservertitle2").innerHTML = x;
+}
+
+function myFunction() {
+  var x = document.getElementById("mySelect").value;
+  document.getElementById("demo").innerHTML = "You selected: " + x;
+}
+
+function myFunction() {
+  var x = document.getElementById("mySelect").value;
+  document.getElementById("demo").innerHTML = "You selected: " + x;
+}
